a91a8269a38cd8abc2f680c12b5180d5141dd28f
Bryan Hankins <bryanhankins@me.com>
2016-07-31 14:11:02 -0500

    stacks logging in console

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var perLine = [];
 
-for (var i = 0; i < alpha.length; i++) {
-  alpha[i]
-console.log(alpha[i])
+function alphaStacks(){
+  for (var i = 0; i < alpha.length; i++) {
+    if(i%5 === 0) {
+      perLine += " " + alpha[i]
+      console.log(perLine)
+    } else {
+      perLine += alpha[i]
+      console.log(perLine)
+    }
+  }
 }
+alphaStacks()
