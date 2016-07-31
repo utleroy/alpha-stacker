b3cbf84e04d58878867677cc25df71d33edfad9d
Bryan Hankins <bryanhankins@me.com>
2016-07-31 14:46:05 -0500

    alphastacker logging correctly

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var perLine = [];
+var output = document.getElementById("output")
 
 function alphaStacks(){
   for (var i = 0; i < alpha.length; i++) {
     if(i%5 === 0) {
       perLine += " " + alpha[i]
-      console.log(perLine)
     } else {
       perLine += alpha[i]
       console.log(perLine)
@@ -13,3 +13,4 @@ function alphaStacks(){
   }
 }
 alphaStacks()
+
