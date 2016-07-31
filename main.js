a61d931b46ac6a3ab5a55d65f4016dae50755982
Bryan Hankins <bryanhankins@me.com>
2016-07-31 13:30:35 -0500

    alpha array logging

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("test")
+var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+for (var i = 0; i < alpha.length; i++) {
+  alpha[i]
+console.log(alpha[i])
+}
